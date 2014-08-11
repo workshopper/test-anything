db193cf50a08311e91d69e8aa23098a5366f8a0b
Finn <derfinn@gmail.com>
2014-08-11 20:17:38 +0200

    style

--- test-anything.js
+++ test-anything.js
@@ -3,17 +3,20 @@
 var adventure = require('adventure')
 var path = require('path')
 
-var shop = adventure('test-anything')
+var shop = adventure({
+  title: 'TEST ANYTHING!',
+  name: 'test-anything'
+})
 
-shop.add('LOG IT OUT', 
+shop.add('» LOG IT OUT', 
   function () { return require('./exercises/log-it-out') })
-shop.add('TELL ME WHAT IS WRONG', 
+shop.add('» TELL ME WHAT IS WRONG', 
   function () { return require('./exercises/tell-me-what-is-wrong') })
-shop.add('TAPE IT TOGETHER',
+shop.add('» TAPE IT TOGETHER',
   function () { return require('./exercises/tape-it-together') })
-shop.add('CALL ME MAYBE',
+shop.add('» CALL ME MAYBE',
   function() { return require('./exercises/call-me-maybe')})
-shop.add('TO ERR IS HUMAN, TO PURR FELINE',
+shop.add('» TO ERR IS HUMAN, TO PURR FELINE',
   function () { return require('./exercises/to-err-is-human')})
 
 shop.execute(process.argv.slice(2))
