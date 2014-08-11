0f1461132b3f0bb156ce8f7481d61c43864c4940
Finn <derfinn@gmail.com>
2014-08-11 20:11:06 +0200

    Big renaming for better reordering

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ For more information about nodeschool see here: http://nodeschool.io/
 
 ## Ideas:
 - Teach about the exit code about the test in exercise 2 or 3
-- Other frameworks than tape?
+- Other frameworks than tape (maybe optional bonus level)
 - travis-ci, appveyor (needs git-it knowledge)
 - pretty printer (faucet, etc.)
 - compare objects
--- test-anything.js
+++ test-anything.js
@@ -6,14 +6,14 @@ var path = require('path')
 var shop = adventure('test-anything')
 
 shop.add('LOG IT OUT', 
-  function () { return require('./exercises/01-log-it-out') })
+  function () { return require('./exercises/log-it-out') })
 shop.add('TELL ME WHAT IS WRONG', 
-  function () { return require('./exercises/02-tell-me-what-is-wrong') })
+  function () { return require('./exercises/tell-me-what-is-wrong') })
 shop.add('TAPE IT TOGETHER',
-  function () { return require('./exercises/03-tape-it-together') })
+  function () { return require('./exercises/tape-it-together') })
 shop.add('CALL ME MAYBE',
-  function() { return require('./exercises/04-call-me-maybe')})
+  function() { return require('./exercises/call-me-maybe')})
 shop.add('TO ERR IS HUMAN, TO PURR FELINE',
-  function () { return require('./exercises/05-to-err-is-human')})
+  function () { return require('./exercises/to-err-is-human')})
 
 shop.execute(process.argv.slice(2))
