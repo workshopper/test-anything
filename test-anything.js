fa259ea98c0c91710ffcac168792a41f9a8435ce
finnp <derfinn@gmail.com>
2016-11-17 16:51:59 +0100

    fix: do not show deprecation warnings to users, fixes #29

--- test-anything.js
+++ test-anything.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-deprecation
 
 var adventure = require('adventure')
 var path = require('path')
@@ -8,9 +8,9 @@ var shop = adventure({
   name: 'test-anything'
 })
 
-shop.add('» LOG IT OUT', 
+shop.add('» LOG IT OUT',
   function () { return require('./exercises/log-it-out') })
-shop.add('» TELL ME WHAT IS WRONG', 
+shop.add('» TELL ME WHAT IS WRONG',
   function () { return require('./exercises/tell-me-what-is-wrong') })
 shop.add('» TAPE IT TOGETHER',
   function () { return require('./exercises/tape-it-together') })
